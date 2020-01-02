457e8fc7572a4a5f27c6ccc440d242f2ddfbd86a
Cat Lover <38192420+Catlover3@users.noreply.github.com>
2020-01-02 10:00:16 +0100

    Start of Schoolproject

---
+++ Spring_assignment/src/main/java/service/StudentManagementConsoleImpl.java
@@ -0,0 +1,31 @@
+package service;
+
+import models.Student;
+
+import java.util.List;
+
+public class StudentManagementConsoleImpl implements StudentManagement {
+    public Student create() {
+        return null;
+    }
+
+    public Student save(Student student) {
+        return null;
+    }
+
+    public Student find(int id) {
+        return null;
+    }
+
+    public Student removeStudent(int id) {
+        return null;
+    }
+
+    public List<Student> findAll() {
+        return null;
+    }
+
+    public Student edit(Student student) {
+        return null;
+    }
+}
